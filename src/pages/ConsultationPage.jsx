cbf71c917a58c97566b92ad88a3a3e3b674684b2
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2023-06-10 20:20:10 +0700

    add screen: consultation page

--- src/pages/ConsultationPage.jsx
+++ src/pages/ConsultationPage.jsx
@@ -1,5 +1,51 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { listPsikolog } from "../data/index";
+import FaqComponent from "../components/FaqComponent";
+
 const ConsultationPage = () => {
-  return <div>ConsultationPage</div>;
+  return (
+    <div className="consultation-page">
+      <div className="consultation min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="fw-bold text-center">All Psikolog</h1>
+              <p className="text-center">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Molestiae, impedit!
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            {listPsikolog.map((psikolog) => {
+              return (
+                <Col key={psikolog.id} className="shadow rounded">
+                  <img
+                    src={psikolog.image}
+                    alt="unsplash.com"
+                    className="w-100 mb-5 rounded-top"
+                  />
+                  <div className="star mb-2 px-3">
+                    <i className={psikolog.star1}></i>
+                    <i className={psikolog.star2}></i>
+                    <i className={psikolog.star3}></i>
+                    <i className={psikolog.star4}></i>
+                    <i className={psikolog.star5}></i>
+                  </div>
+                  <h5 className="mb-5 px-3">{psikolog.name}</h5>
+                  <div className="keterangan d-flex justify-content-between align-items-center px-3 pb-3">
+                    <p className="m-0 text-primary fw-bold">{psikolog.role}</p>
+                    <button className=" rounded-1">{psikolog.detail}</button>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
+      <FaqComponent />
+    </div>
+  );
 };
 
 export default ConsultationPage;
