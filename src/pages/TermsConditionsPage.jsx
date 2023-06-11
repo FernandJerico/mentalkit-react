a059091893c6c0baac1ae8bc470456b2f0e1c99f
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2023-06-11 14:17:32 +0700

    add screen: terms and conditions page, useNavigate

--- src/pages/TermsConditionsPage.jsx
+++ src/pages/TermsConditionsPage.jsx
@@ -1,5 +1,127 @@
+import { Container, Row, Col } from "react-bootstrap";
+import FaqComponent from "../components/FaqComponent";
+
 const TermsConditionsPage = () => {
-  return <div>TermsConditionsPage</div>;
+  return (
+    <div>
+      <div className="terms-page">
+        <div className="terms min-vh-100">
+          <Container>
+            <Row>
+              <Col>
+                <h1 className="text-center fw-bold mb-2">
+                  Terms and Conditions
+                </h1>
+                <p className="text-center">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut,
+                  fugit!
+                </p>
+              </Col>
+            </Row>
+            <Row className="pt-5">
+              <Col>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+                  impedit odio quisquam eos debitis. Facilis officiis, autem
+                  asperiores voluptatum eos magni inventore dicta aliquam
+                  veniam. Harum ducimus optio natus atque explicabo dicta,
+                  excepturi quae repellendus error impedit adipisci pariatur ut
+                  consectetur. Nihil quod illum earum ratione vel aut alias
+                  quae.
+                </p>
+              </Col>
+            </Row>
+            <Row className="py-3">
+              <Col>
+                <h4 className="fw-bold">1. Lorem</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Necessitatibus ipsam ipsum, nemo molestiae eum adipisci odit,
+                  eveniet veritatis reiciendis, ratione deserunt earum possimus
+                  aspernatur quia. Perferendis repellendus obcaecati laborum?
+                  Nihil sed asperiores sapiente quo consequatur numquam
+                  inventore voluptatum debitis quasi.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis eligendi sequi eaque iusto rem blanditiis quisquam
+                  deserunt dicta molestias molestiae debitis, praesentium,
+                  expedita illo. Vero velit qui repellat totam rem magni eos
+                  illo. Reprehenderit, cum, minima unde dolore et qui odio
+                  assumenda similique magnam beatae ullam? Quam, voluptatibus
+                  culpa a in cupiditate accusamus ab tempore, debitis itaque
+                  ratione rerum cumque.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Mollitia, fugit quos labore voluptatibus reprehenderit minus
+                  expedita illum quam culpa similique non! Eius aut et facere ut
+                  tempora? Tempore temporibus, magni ipsum deserunt obcaecati,
+                  quam esse facere aspernatur nulla maiores asperiores ex animi?
+                  Magnam aliquam error eaque consequatur nam? Labore culpa iusto
+                  illo quas aperiam sunt delectus expedita ratione ullam omnis
+                  nemo molestiae error repellat deserunt id nostrum beatae
+                  provident nisi dolor aliquid, officiis unde! Repudiandae,
+                  alias possimus? Sit, quo enim.
+                </p>
+              </Col>
+            </Row>
+            <Row className="py-3">
+              <Col>
+                <h4 className="fw-bold">2. Lorem</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laborum sed aliquid nam sint excepturi aliquam dicta ut,
+                  consequuntur blanditiis sequi molestiae numquam nihil fugiat
+                  porro mollitia voluptas quo rerum. Sunt a ipsa quis
+                  accusantium tempora ullam animi? Magni, voluptate accusantium!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                  ratione ullam ex nam neque maiores? Illo dolorem, fuga sequi
+                  iusto deserunt excepturi minus earum molestiae nobis ipsum
+                  voluptates modi incidunt.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic
+                  sed beatae, error expedita veniam quam alias omnis fuga,
+                  maxime cupiditate magni at atque neque quasi eaque aut rem
+                  ratione placeat maiores sapiente itaque necessitatibus
+                  possimus dolores? Voluptatum consequuntur unde nemo itaque
+                  adipisci commodi ipsa perferendis excepturi, assumenda
+                  consequatur dignissimos? Alias.
+                </p>
+              </Col>
+            </Row>
+            <Row className="py-3">
+              <Col>
+                <h4 className="fw-bold">3. Lorem</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Similique delectus deleniti iste iure numquam, fugiat alias
+                  quibusdam animi nulla vero dolorem quod maxime reiciendis
+                  minima neque ipsum sit eos esse exercitationem odit nam illo.
+                  Aut cumque ab voluptatibus dolorum, quae, minima facere
+                  quaerat veniam quasi molestiae ratione quia iusto enim
+                  asperiores rem? Possimus aliquam dicta dolore velit
+                  perspiciatis! Architecto officiis in consectetur reiciendis
+                  quia quasi facilis quis atque quaerat eos.
+                </p>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
+                  impedit, ut doloremque aspernatur dolorem, neque asperiores
+                  officia aut mollitia a voluptatem beatae itaque, corrupti
+                  quibusdam accusamus at distinctio perspiciatis eaque fugiat
+                  libero expedita odio id molestiae illo. Eaque, cum facere.
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+      <FaqComponent />
+    </div>
+  );
 };
 
 export default TermsConditionsPage;
